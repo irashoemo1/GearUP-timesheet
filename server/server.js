c36843396221f6813594e21b86843e2289f08cad
Ira Shoemo <irashoemo1@gmail.com>
2023-02-23 20:28:59 -0500

    changing up

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function(req,res) {
-        res.sendFile(path.join(__dirname, 'build', 'public/index.html'));
+        res.sendFile(path.join(__dirname, 'build', '/index.html'));
 });
 
 app.use("/auth", authRoutes)
